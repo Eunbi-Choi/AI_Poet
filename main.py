841101d2d79c05ded06559a13742d87bc393846f
Eunbi-Choi <147282133+Eunbi-Choi@users.noreply.github.com>
2025-12-07 03:08:47 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 #pip install langchain-openai
 #pip install streamlit
 
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 from langchain_openai import ChatOpenAI
 chat_model = ChatOpenAI()
@@ -13,6 +13,7 @@ chat_model = ChatOpenAI()
 # print(result.content)
 
 import streamlit as st
+
 st.title("인공지능 시인")
 subject = st.text_input("시의 주제를 입력해주세요.")
 st.write("시의 주제 : " + subject)
